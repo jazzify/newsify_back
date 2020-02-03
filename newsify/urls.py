4508bdfa798979654201165cfc2c1dd1932f653e
= <=>
2020-02-03 11:23:42 -0500

    Squashed commit of the following:
    commit ec4aeab747da290d03987baf161e593bd0659a66
    Author: = <=>
    Date:   Mon Feb 3 11:19:44 2020 -0500
    
        add and config django-debug-toolbar

--- newsify/urls.py
+++ newsify/urls.py
@@ -1,7 +1,9 @@
-from rest_framework import routers
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
 from scraper.views import PostViewSet
+from newsify import settings
 
 router = routers.DefaultRouter()
 router.register(r"posts", PostViewSet)
@@ -11,3 +13,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
