d69335d3f159d351a0e22104390f5950765d4cb9
= <=>
2020-02-05 16:13:09 -0500

    send an email when scrape fails

--- scraper/management/commands/scrape.py
+++ scraper/management/commands/scrape.py
@@ -1,4 +1,9 @@
+import os
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 from scraper.services import ScraperManager
 
 class Command(BaseCommand):
@@ -7,5 +12,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         scraper_manager = ScraperManager()
         errors = scraper_manager.run_requests()
+
+        if not settings.DEBUG and errors:
+            message = Mail(
+                from_email='support@newsify.com',
+                to_emails=os.environ.get('DEVELOPERS_SUPPORT_EMAIL'),
+                subject='Scrape got some errors',
+                html_content=f'{errors}')
+            try:
+                sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+                sg.send(message)
+            except Exception as error:
+                print(str(error))
 
         self.stdout.write(self.style.ERROR(f'Scraping errors "{errors}"'))
